f2e8840ec90735daf573f3c80826c94c5358383c
Akshat1507 <patelakshat8801@gmail.com>
2023-04-05 09:57:29 +0530

    class task

---
+++ typescript/classtask.ts
@@ -0,0 +1,16 @@
+class Person{
+    name : string;
+    age : number;
+    address : string
+    constructor(name: string, age:number, address:string){
+        this.name = name,
+        this.age = age,
+        this.address = address
+    }
+    get(){
+
+    }
+    set(){
+        
+    }
+}
